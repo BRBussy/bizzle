ce7a6944ca0f2c20c7938728fded1cd02b89af6d
Bernard Bussy <brbitzbussy@gmail.com>
2019-09-07 15:03:33 +0200

    start giving gateway access to authenticator

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"cloud.google.com/go/compute/metadata"
 	"fmt"
 	"github.com/BRBussy/bizzle/package/authenticator"
 	"github.com/sirupsen/logrus"
@@ -15,7 +16,26 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if target == "" {
 		target = "World"
 	}
+
 	fmt.Fprintf(w, "Hello from gateway yet again!!! %s!\n", target)
+}
+
+// makeGetRequest makes a GET request to the specified Cloud Run endpoint in
+// serviceURL (must be a complete URL) by authenticating with the ID token
+// obtained from the Metadata API.
+func makeGetRequest(serviceURL string) (*http.Response, error) {
+	// query the id_token with ?audience as the serviceURL
+	tokenURL := fmt.Sprintf("/instance/service-accounts/default/identity?audience=%s", serviceURL)
+	idToken, err := metadata.Get(tokenURL)
+	if err != nil {
+		return nil, fmt.Errorf("metadata.Get: failed to query id_token: %+v", err)
+	}
+	req, err := http.NewRequest("GET", serviceURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", idToken))
+	return http.DefaultClient.Do(req)
 }
 
 func main() {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/BRBussy/bizzle
 
 go 1.12
 
-require github.com/sirupsen/logrus v1.4.2
+require (
+	cloud.google.com/go v0.45.1
+	github.com/sirupsen/logrus v1.4.2
+)
