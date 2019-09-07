a50aea5a067d5b943da34c81c1b9ed5f51cde8f0
Bernard Bussy <brbitzbussy@gmail.com>
2019-09-07 21:56:17 +0200

    starting config for authenticator

--- cmd/authenticator/main.go
+++ cmd/authenticator/main.go
@@ -1,33 +1,61 @@
 package main
 
 import (
-	"fmt"
-	"github.com/BRBussy/bizzle/package/authenticator"
-	"github.com/sirupsen/logrus"
-	"log"
-	"net/http"
+	"flag"
+	gatewayConfig "github.com/BRBussy/bizzle/configs/gateway"
+	jsonRpcHttpServer "github.com/BRBussy/bizzle/internal/pkg/api/jsonRpc/server/http"
+	jsonRpcServiceProvider "github.com/BRBussy/bizzle/internal/pkg/api/jsonRpc/service/provider"
+	authenticatorJsonRpcAdaptor "github.com/BRBussy/bizzle/package/authenticator/adaptor/jsonRpc"
+	basicAuthenticator "github.com/BRBussy/bizzle/package/authenticator/basic"
+	"github.com/rs/zerolog/log"
 	"os"
+	"os/signal"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	log.Print("Authenticator received a request.")
-	target := os.Getenv("TARGET")
-	if target == "" {
-		target = "World"
-	}
-	fmt.Fprintf(w, "Hello from authenticator !!! %s!\n", target)
-}
+var configFileName = flag.String("config-file-name", "config", "specify config file")
 
 func main() {
-	logrus.Info("The bizzle authenticator has started!")
-	authenticator.Auth()
+	flag.Parse()
 
-	http.HandleFunc("/", handler)
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
+	// get gateway config
+	config, err := gatewayConfig.GetConfig(configFileName)
+	if err != nil {
+		log.Fatal().Err(err).Msg("getting config from file")
 	}
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	// create service providers
+	BasicAuthenticator := basicAuthenticator.New()
+
+	// create rpc http server
+	server := jsonRpcHttpServer.New(
+		"/",
+		"0.0.0.0",
+		config.ServerPort,
+	)
+
+	// register service providers
+	if err := server.RegisterBatchServiceProviders([]jsonRpcServiceProvider.Provider{
+		authenticatorJsonRpcAdaptor.New(BasicAuthenticator),
+	}); err != nil {
+		log.Fatal().Err(err).Msg("registering batch service providers")
+	}
+
+	// start server
+	log.Info().Msgf("starting authenticator json rpc http api server started on port %s", config.ServerPort)
+	go func() {
+		if err := server.Start(); err != nil {
+			log.Error().Err(err).Msg("json rpc http api server has stopped")
+		}
+	}()
+
+	// wait for interrupt signal to stop
+	systemSignalsChannel := make(chan os.Signal, 1)
+	signal.Notify(systemSignalsChannel, os.Interrupt)
+	for {
+		select {
+		case s := <-systemSignalsChannel:
+			log.Info().Msgf("Application is shutting down.. ( %s )", s)
+			return
+		}
+	}
 }
