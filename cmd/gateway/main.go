9cff8596f6f9e4ca6c0ded6d12bd342ee5898172
Bernard Bussy <brbitzbussy@gmail.com>
2019-09-07 22:38:00 +0200

    setting service access token

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -4,15 +4,14 @@ import (
 	"cloud.google.com/go/compute/metadata"
 	"flag"
 	"fmt"
-	"github.com/BRBussy/bizzle/configs/authenticator"
 	gatewayConfig "github.com/BRBussy/bizzle/configs/gateway"
 	basicJsonRpcClient "github.com/BRBussy/bizzle/internal/pkg/api/jsonRpc/client/basic"
 	jsonRpcHttpServer "github.com/BRBussy/bizzle/internal/pkg/api/jsonRpc/server/http"
 	jsonRpcServiceProvider "github.com/BRBussy/bizzle/internal/pkg/api/jsonRpc/service/provider"
 	authenticatorJsonRpcAdaptor "github.com/BRBussy/bizzle/internal/pkg/authenticator/adaptor/jsonRpc"
 	jsonRpcAuthenticator "github.com/BRBussy/bizzle/internal/pkg/authenticator/jsonRpc"
+	"github.com/BRBussy/bizzle/internal/pkg/environment"
 	"github.com/rs/zerolog/log"
-	"net/http"
 	"os"
 	"os/signal"
 )
@@ -30,8 +29,16 @@ func main() {
 
 	// create json rpc client to access the authenticator
 	BasicJsonRpcClient := basicJsonRpcClient.New(config.AuthenticatorURL)
-	if config.Environment == authenticator.ProductionEnvironment {
+	if config.Environment == environment.Production {
+		// query the id_token with ?audience as the authentication serviceURL
+		authenticationServiceToken, err := metadata.Get(fmt.Sprintf("/instance/service-accounts/default/identity?audience=%s", config.AuthenticatorURL))
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to get authentication service access token")
+		}
+		log.Info().Msg("obtained authentication service access token")
 
+		// set this as an additional header
+		BasicJsonRpcClient.AddAdditionalHeaderEntry("Authorization", fmt.Sprintf("Bearer %s", authenticationServiceToken))
 	}
 
 	// create service providers
@@ -69,47 +76,4 @@ func main() {
 			return
 		}
 	}
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	log.Info().Msg("Gateway received a request.")
-
-	response, err := makeGetRequest("/")
-	if err != nil {
-		fmt.Fprintf(w, "could not get to authenticator :( %s!\n", err.Error())
-		log.Error().Err(err).Msg("could not get to authenticator!! ")
-		return
-	}
-
-	var bodyBytes []byte
-	if _, err := response.Body.Read(bodyBytes); err != nil {
-		log.Error().Err(err).Msg("error reading response body! ")
-		fmt.Fprintf(w, "could not read authenticators body bytes :( %s!\n", err.Error())
-		return
-	}
-
-	fmt.Fprintf(w, "Response from authenticator!!!!! %s!\n", string(bodyBytes))
-}
-
-// makeGetRequest makes a GET request to the specified Cloud Run endpoint in
-// serviceURL (must be a complete URL) by authenticating with the ID token
-// obtained from the Metadata API.
-func makeGetRequest(serviceURL string) (*http.Response, error) {
-	// create a request
-	req, err := http.NewRequest("GET", serviceURL, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	if "prod" == "prod" {
-		// query the id_token with ?audience as the serviceURL
-		tokenURL := fmt.Sprintf("/instance/service-accounts/default/identity?audience=%s", serviceURL)
-		idToken, err := metadata.Get(tokenURL)
-		if err != nil {
-			return nil, fmt.Errorf("metadata.Get: failed to query id_token: %+v", err)
-		}
-		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", idToken))
-	}
-
-	return http.DefaultClient.Do(req)
 }
