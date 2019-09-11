1e6bc5d3fd686529c4a3c1489a8fd37c109986d4
Bernard Bussy <brbitzbussy@gmail.com>
2019-09-11 11:16:27 +0200

    add or criterion

---
+++ pkg/search/criterion/or/criterion.go
@@ -0,0 +1,33 @@
+package or
+
+import (
+	"errors"
+	"github.com/BRBussy/bizzle/pkg/search/criterion"
+)
+
+const Type = criterion.Or
+
+type Criterion struct {
+	Criteria []criterion.Criterion
+}
+
+func (c Criterion) IsValid() error {
+	if len(c.Criteria) == 0 {
+		return errors.New("no criteria to or together")
+	}
+	return nil
+}
+
+func (c Criterion) Type() criterion.Type {
+	return Type
+}
+
+func (c Criterion) ToFilter() map[string]interface{} {
+	filter := make(map[string]interface{})
+	criteriaFilters := make([]map[string]interface{}, 0)
+	for _, crit := range c.Criteria {
+		criteriaFilters = append(criteriaFilters, crit.ToFilter())
+	}
+	filter["$or"] = criteriaFilters
+	return filter
+}
