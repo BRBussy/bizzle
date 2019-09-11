1af854914334faeea26c91ac9b7e9d4d7b51d8de
Bernard Bussy <brbitzbussy@gmail.com>
2019-09-11 08:43:33 +0200

    use authenticated json rpc client or basic

--- internal/pkg/exercise/store/jsonRpc/store.go
+++ internal/pkg/exercise/store/jsonRpc/store.go
@@ -2,7 +2,11 @@ package jsonRpc
 
 import (
 	jsonRpcClient "github.com/BRBussy/bizzle/internal/pkg/api/jsonRpc/client"
+	authenticatedJsonRpcClient "github.com/BRBussy/bizzle/internal/pkg/api/jsonRpc/client/authenticated"
+	basicJsonRpcClient "github.com/BRBussy/bizzle/internal/pkg/api/jsonRpc/client/basic"
 	brizzleAuthenticatorJsonRpcAdaptor "github.com/BRBussy/bizzle/internal/pkg/authenticator/adaptor/jsonRpc"
+	"github.com/BRBussy/bizzle/internal/pkg/environment"
+	bizzleException "github.com/BRBussy/bizzle/internal/pkg/exception"
 	exerciseStore "github.com/BRBussy/bizzle/internal/pkg/exercise/store"
 	"github.com/rs/zerolog/log"
 )
@@ -12,11 +16,26 @@ type store struct {
 }
 
 func New(
-	jsonRpcClient jsonRpcClient.Client,
-) exerciseStore.Store {
-	return &store{
-		jsonRpcClient: jsonRpcClient,
+	env environment.Environment,
+	authenticatorURL string,
+) (exerciseStore.Store, error) {
+	var client jsonRpcClient.Client
+	var err error
+	switch env {
+	case environment.Production:
+		client = basicJsonRpcClient.New(authenticatorURL)
+	case environment.Development:
+		client, err = authenticatedJsonRpcClient.New(authenticatorURL)
+		if err != nil {
+			log.Error().Err(err).Msg("creating new authenticated json rpc client")
+			return nil, err
+		}
+	default:
+		return nil, bizzleException.ErrUnexpected{Reasons: []string{"invalid environment", env.String()}}
 	}
+	return &store{
+		jsonRpcClient: client,
+	}, nil
 }
 
 func (a *store) Find(request *exerciseStore.FindRequest) (*exerciseStore.FindResponse, error) {
