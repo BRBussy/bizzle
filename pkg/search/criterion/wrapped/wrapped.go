dcd3167a89271980fbb7b63f060c505bd5e63525
Bernard Bussy <brbitzbussy@gmail.com>
2019-09-11 07:38:02 +0200

    unwrapping method complete

---
+++ pkg/search/criterion/wrapped/wrapped.go
@@ -0,0 +1,38 @@
+package wrapped
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/BRBussy/bizzle/pkg/search/criterion"
+	"github.com/BRBussy/bizzle/pkg/search/criterion/substring"
+)
+
+type Wrapped struct {
+	Type  criterion.Type  `json:"type"`
+	Value json.RawMessage `json:"value"`
+}
+
+func (w Wrapped) Unwrap() (criterion.Criterion, error) {
+	var result criterion.Criterion = nil
+
+	switch w.Type {
+	case criterion.Substring:
+		var unmarshalledCriterion substring.Criterion
+		if err := json.Unmarshal(w.Value, &unmarshalledCriterion); err != nil {
+			return nil, errors.New("unmarshalling failed: " + err.Error())
+		}
+		result = unmarshalledCriterion
+	default:
+		return nil, errors.New("invalid/unsupported criterion type")
+	}
+
+	if result == nil {
+		return nil, errors.New("criterion still nil")
+	}
+
+	if err := result.IsValid(); err != nil {
+		return nil, errors.New("criterion not valid: " + err.Error())
+	}
+
+	return result, nil
+}
