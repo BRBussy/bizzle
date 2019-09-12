0b4c6dc36404b51371d0a0743ef68d6648f01d65
Bernard Bussy <brbitzbussy@gmail.com>
2019-09-12 21:00:50 +0200

    test substring exact criterion

--- pkg/search/criteria/serializedTest.go
+++ pkg/search/criteria/serializedTest.go
@@ -3,6 +3,7 @@ package criteria
 import (
 	"fmt"
 	searchCriterion "github.com/BRBussy/bizzle/pkg/search/criterion"
+	operationCriterion "github.com/BRBussy/bizzle/pkg/search/criterion/operation"
 	stringCriterion "github.com/BRBussy/bizzle/pkg/search/criterion/string"
 	"github.com/stretchr/testify/suite"
 )
@@ -10,10 +11,9 @@ import (
 type testCase struct {
 	serializedCriterion []byte
 	criterion           searchCriterion.Criterion
-	expectedError       error
 }
 
-var substring1 = testCase{
+var stringSubstring1 = testCase{
 	serializedCriterion: []byte(fmt.Sprintf(
 		"{\"someField\":{\"type\":\"%s\",\"string\":\"someSubstring\"}}",
 		searchCriterion.StringSubstringCriterionType,
@@ -22,7 +22,17 @@ var substring1 = testCase{
 		Field:  "someField",
 		String: "someSubstring",
 	},
-	expectedError: nil,
+}
+
+var stringExact1 = testCase{
+	serializedCriterion: []byte(fmt.Sprintf(
+		"{\"someField\":{\"type\":\"%s\",\"string\":\"someExactString\"}}",
+		searchCriterion.StringExactCriterionType,
+	)),
+	criterion: stringCriterion.Exact{
+		Field:  "someField",
+		String: "someExactString",
+	},
 }
 
 type serializedTest struct {
@@ -84,7 +94,7 @@ func (t serializedTest) TestSerializedCriteriaOROperatorFailures() {
 	)
 }
 
-func (t serializedTest) TestFieldCriterionFailures() {
+func (t serializedTest) TestSerializedCriteriaFieldCriterionFailures() {
 	// invalid input for field criterion
 	t.Equal(
 		ErrUnmarshal{Reasons: []string{
@@ -125,16 +135,56 @@ func (t serializedTest) TestFieldCriterionFailures() {
 	)
 }
 
-func (t serializedTest) TestStringSubstringCriterion() {
+func (t serializedTest) TestSerializedCriteriaStringSubstringCriterion() {
 	testSubstringCriterion := Serialized{}
 	t.Equal(
 		nil,
-		(&testSubstringCriterion).UnmarshalJSON(substring1.serializedCriterion),
+		(&testSubstringCriterion).UnmarshalJSON(stringSubstring1.serializedCriterion),
 	)
 	t.Equal(
 		[]searchCriterion.Criterion{
-			substring1.criterion,
+			stringSubstring1.criterion,
 		},
 		testSubstringCriterion.Criteria,
+	)
+}
+
+func (t serializedTest) TestSerializedCriteriaStringExactCriterion() {
+	testSubstringCriterion := Serialized{}
+	t.Equal(
+		nil,
+		(&testSubstringCriterion).UnmarshalJSON(stringExact1.serializedCriterion),
+	)
+	t.Equal(
+		[]searchCriterion.Criterion{
+			stringExact1.criterion,
+		},
+		testSubstringCriterion.Criteria,
+	)
+}
+
+func (t serializedTest) TestSerializedCriteriaOrCriterion() {
+	serializedValue := []byte(fmt.Sprintf(
+		"{\"$or\":[%s,%s]}",
+		stringSubstring1.serializedCriterion,
+		stringExact1.serializedCriterion,
+	))
+
+	testOrCriterion := Serialized{}
+
+	t.Equal(
+		nil,
+		(&testOrCriterion).UnmarshalJSON(serializedValue),
+	)
+	t.Equal(
+		[]searchCriterion.Criterion{
+			operationCriterion.Or{
+				Criteria: []searchCriterion.Criterion{
+					stringSubstring1.criterion,
+					stringExact1.criterion,
+				},
+			},
+		},
+		testOrCriterion.Criteria,
 	)
 }
