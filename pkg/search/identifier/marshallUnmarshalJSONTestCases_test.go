e12b870a2a3d7d1df567929ffcf3975274abc4b6
Bernard Bussy <brbitzbussy@gmail.com>
2019-09-18 13:28:50 +0200

    started json unmarshall test

---
+++ pkg/search/identifier/marshallUnmarshalJSONTestCases_test.go
@@ -0,0 +1,18 @@
+package identifier
+
+import "fmt"
+
+type TestCase struct {
+	id                   string
+	SerializedIdentifier []byte
+	Identifier           Identifier
+}
+
+var idIdentifierTestCase0 = TestCase{
+	id: "idIdentifierTestCase0",
+	SerializedIdentifier: []byte(fmt.Sprintf(
+		"{\"type\":\"%s\",\"id\":\"1234\"}",
+		IDIdentifierType,
+	)),
+	Identifier: ID("1234"),
+}
--- pkg/search/identifier/unmarshallJSON_test.go
+++ pkg/search/identifier/unmarshallJSON_test.go
@@ -1,1 +1,27 @@
 package identifier
+
+import (
+	testifyAssert "github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestUnmarshalSerializedIdentifier_InvalidInput(t *testing.T) {
+	assert := testifyAssert.New(t)
+
+	assert.EqualError(
+		(&Serialized{}).UnmarshalJSON(nil),
+		ErrInvalidSerializedIdentifier{Reasons: []string{"json identifier data is nil"}}.Error(),
+		"error should be correct for nil input",
+	)
+
+	assert.EqualError(
+		(&Serialized{}).UnmarshalJSON([]byte("notValidJSON")),
+		ErrUnmarshal{
+			Reasons: []string{
+				"json unmarshal",
+				"invalid character 'o' in literal null (expecting 'u')",
+			},
+		}.Error(),
+		"error should be correct for incorrect input",
+	)
+}
