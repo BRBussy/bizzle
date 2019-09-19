e9fef2be0dc46e2fe6d819f9d3a1076b44bea5c3
Bernard Bussy <brbitzbussy@gmail.com>
2019-09-19 08:24:48 +0200

    add firebase app

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	cloud.google.com/go v0.45.1
+	firebase.google.com/go v3.9.0+incompatible
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/gorilla/mux v1.7.3
 	github.com/gorilla/rpc v1.2.0
@@ -15,5 +16,7 @@ require (
 	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
 	github.com/xdg/stringprep v1.0.0 // indirect
 	go.mongodb.org/mongo-driver v1.1.1
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
+	google.golang.org/api v0.9.0
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
--- internal/pkg/firebase/firebase.go
+++ internal/pkg/firebase/firebase.go
@@ -1,8 +1,26 @@
 package firebase
 
+import (
+	"fmt"
+
+	"golang.org/x/net/context"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+)
+
 type Firebase struct {
+	app *firebase.App
 }
 
-func New() *Firebase {
-	return &Firebase{}
+func New(firebaseCredentialsPath string) (*Firebase, error) {
+	opt := option.WithCredentialsFile(firebaseCredentialsPath)
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		return nil, fmt.Errorf("error initializing app: %v", err)
+	}
+
+	return &Firebase{
+		app: app,
+	}, nil
 }
