7b11f42a2ee84e257f7bae75cebfc614897400a9
Bernard Bussy <brbitzbussy@gmail.com>
2019-09-19 07:33:47 +0200

    add user admin interface

--- internal/pkg/user/admin/admin.go
+++ internal/pkg/user/admin/admin.go
@@ -1,1 +1,29 @@
 package admin
+
+import (
+	"github.com/BRBussy/bizzle/internal/pkg/user"
+)
+
+type Admin interface {
+	CreateOne(*CreateOneRequest) (*CreateOneResponse, error)
+	UpdateOne(*UpdateOneRequest) (*UpdateOneResponse, error)
+}
+
+const ServiceProvider = "User-Store"
+
+const CreateOneService = ServiceProvider + ".CreateOne"
+const UpdateOneService = ServiceProvider + ".UpdateOne"
+
+type CreateOneRequest struct {
+	User user.User
+}
+
+type CreateOneResponse struct {
+}
+
+type UpdateOneRequest struct {
+	User user.User
+}
+
+type UpdateOneResponse struct {
+}
