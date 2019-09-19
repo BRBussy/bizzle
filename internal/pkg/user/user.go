861b906d5a5ee0adae99f8426656d09aba4d3956
Bernard Bussy <brbitzbussy@gmail.com>
2019-09-19 07:14:36 +0200

    add user entity

---
+++ internal/pkg/user/user.go
@@ -0,0 +1,10 @@
+package user
+
+import "github.com/BRBussy/bizzle/pkg/search/identifier"
+
+type User struct {
+	ID           string          `json:"id" bson:"id"`
+	Name         string          `json:"name" bson:"name"`
+	EmailAddress string          `json:"emailAddress" bson:"emailAddress"`
+	RoleIDs      []identifier.ID `json:"roleIDs" bson:"roleIDs"`
+}
