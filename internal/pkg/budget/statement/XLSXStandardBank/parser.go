6702b91954533f16d70578f62dc72011562b646a
Bernard Bussy <brbitzbussy@gmail.com>
2020-01-21 20:42:57 +0200

    parse done

--- internal/pkg/budget/statement/XLSXStandardBank/parser.go
+++ internal/pkg/budget/statement/XLSXStandardBank/parser.go
@@ -2,6 +2,7 @@ package XLSXStandardBank
 
 import (
 	"fmt"
+	"github.com/BRBussy/bizzle/internal/pkg/budget"
 	"github.com/BRBussy/bizzle/internal/pkg/budget/statement"
 	"github.com/rs/zerolog/log"
 	"github.com/tealeg/xlsx"
@@ -54,14 +55,16 @@ func (p Parser) ParseStatement(request *statement.ParseStatementRequest) (*state
 		reasonsInvalid = append(reasonsInvalid, "could not find starting year")
 	}
 
+	// return if the sheet is invalid
 	if len(reasonsInvalid) > 0 {
 		return nil, ErrSheetInvalid{Reasons: reasonsInvalid}
 	}
 
 	// sheet appears valid, parse the rest
+	parsedBudgetEntries := make([]budget.Entry, 0)
 	for rowIdx := range transactionsSheet.Rows[2:] {
 		// check if this is a year row and update year if it is
-		potentialYear, err := transactionsSheet.Rows[1].Cells[colHeaderIndex[DateColumnHeader]].Int()
+		potentialYear, err := transactionsSheet.Rows[2:][rowIdx].Cells[colHeaderIndex[DateColumnHeader]].Int()
 		if err == nil {
 			// assume this is a year row
 			// update year
@@ -72,18 +75,63 @@ func (p Parser) ParseStatement(request *statement.ParseStatementRequest) (*state
 
 		// otherwise assume this is an entry row, try and parse the date
 		entryDate, err := time.Parse(
-			"",
+			"02 Jan 2006",
 			fmt.Sprintf(
 				"%s %s",
-				"",
+				transactionsSheet.Rows[2:][rowIdx].Cells[colHeaderIndex[DateColumnHeader]].String(),
 				strconv.Itoa(year),
 			),
 		)
 		if err != nil {
-			reasonsInvalid = append(reasonsInvalid, fmt.Sprintf("could not parse date in row %i", rowIdx+1))
+			reasonsInvalid = append(reasonsInvalid, fmt.Sprintf("could not parse date in row %d", rowIdx+3))
+			continue
 		}
 
-		// parse
+		// try and parse the out and in amounts
+		inAmountCell := transactionsSheet.Rows[2:][rowIdx].Cells[colHeaderIndex[InColumnHeader]]
+		outAmountCell := transactionsSheet.Rows[2:][rowIdx].Cells[colHeaderIndex[OutColumnHeader]]
+		var amount float64
+		if inAmountCell.String() == "" && outAmountCell.String() == "" {
+			// both are blank
+			reasonsInvalid = append(reasonsInvalid, fmt.Sprintf("both in and out amounts are blank in row %d", rowIdx+3))
+			continue
+		} else if inAmountCell.String() != "" && outAmountCell.String() == "" {
+			// only in value set, try and parse
+			amount, err = inAmountCell.Float()
+			if err != nil {
+				reasonsInvalid = append(reasonsInvalid, fmt.Sprintf("could not parse in amount in row %d", rowIdx+3))
+				continue
+			}
+		} else if inAmountCell.String() == "" && outAmountCell.String() != "" {
+			// only out value set, try and parse
+			amount, err = outAmountCell.Float()
+			if err != nil {
+				reasonsInvalid = append(reasonsInvalid, fmt.Sprintf("could not parse out amount in row %d", rowIdx+3))
+				continue
+			}
+		} else {
+			// both are set
+			reasonsInvalid = append(reasonsInvalid, fmt.Sprintf("both in and out amount set in row %d", rowIdx+3))
+			continue
+		}
+
+		parsedBudgetEntries = append(
+			parsedBudgetEntries,
+			budget.Entry{
+				Date:        entryDate.Unix(),
+				Description: transactionsSheet.Rows[2:][rowIdx].Cells[colHeaderIndex[DescriptionColumnHeader]].String(),
+				Amount:      amount,
+			},
+		)
+	}
+
+	// return if parsing any rows failed
+	if len(reasonsInvalid) > 0 {
+		return nil, ErrSheetInvalid{Reasons: reasonsInvalid}
+	}
+
+	for _, item := range parsedBudgetEntries {
+		fmt.Printf("%v\n", item)
 	}
 
 	return &statement.ParseStatementResponse{}, nil
