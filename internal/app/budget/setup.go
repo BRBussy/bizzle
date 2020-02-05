0485c9ebf73333d522d7b1d29cb821ca818a73a9
Bernard Bussy <brbitzbussy@gmail.com>
2020-02-05 11:20:59 +0200

    remove unnecessary

--- internal/app/budget/setup.go
+++ internal/app/budget/setup.go
@@ -1,1 +1,51 @@
 package budget
+
+import budgetEntryCategoryRule "github.com/BRBussy/bizzle/internal/pkg/budget/entry/categoryRule"
+
+var budgetCategoryRulesToMake = []budgetEntryCategoryRule.CategoryRule{
+	{
+		CategoryIdentifiers: []string{
+			"electricity",
+			"fee",
+		},
+		Category: "Electricity",
+		Strict:   true,
+	},
+	{
+		CategoryIdentifiers: []string{
+			"wesbank",
+		},
+		Category: "CarRepayment",
+	},
+	{
+		CategoryIdentifiers: []string{
+			"vod",
+			"prepaid",
+		},
+		Category: "CellphoneAirtimeData",
+		Strict:   true,
+	},
+	{
+		CategoryIdentifiers: []string{
+			"telkommobi",
+		},
+		Category: "Internet",
+	},
+	{
+		CategoryIdentifiers: []string{
+			"disc",
+			"prem",
+			"medical",
+		},
+		Category: "MedicalAid",
+		Strict:   true,
+	},
+	{
+		CategoryIdentifiers: []string{
+			"salary",
+			"andile",
+		},
+		Category: "Salary",
+		Strict:   true,
+	},
+}
--- internal/pkg/budget/entry/categoryRule/categoryRule.go
+++ internal/pkg/budget/entry/categoryRule/categoryRule.go
@@ -2,119 +2,12 @@ package categoryRule
 
 import (
 	"github.com/BRBussy/bizzle/pkg/search/identifier"
-	"strings"
 )
-
-type Category string
-
-const (
-	OtherCategory                Category = "Other"
-	ElectricityCategory          Category = "Electricity"
-	CarRepaymentCategory         Category = "Car Repayment"
-	CellphoneAirtimeDataCategory Category = "Cellphone Airtime Data"
-	InternetCategory             Category = "Internet"
-	MedicalAidCategory           Category = "Medical Aid"
-	SalaryCategory               Category = "Salary"
-)
-
-type CategoryIdentifier string
-
-func (c CategoryIdentifier) String() string {
-	return string(c)
-}
 
 type CategoryRule struct {
 	ID                  identifier.ID
 	OwnerID             identifier.ID
-	CategoryIdentifiers []CategoryIdentifier
-	Category            Category
+	CategoryIdentifiers []string
+	Category            string
 	Strict              bool
-}
-
-func Categorise(description string) (Category, []CategoryIdentifier, error) {
-
-	// minimise and strip description
-	description = strings.ToLower(strings.Trim(description, " "))
-
-	// cannot categorise blank description
-	if description == "" {
-		return "", nil, ErrCouldNotClassify{Reason: "blank description"}
-	}
-
-nextCategorisationRule:
-	for _, rule := range CategorisationRules {
-		if rule.Strict {
-			// all identifiers must be found in description
-			for _, id := range rule.CategoryIdentifiers {
-				if !strings.Contains(description, id.String()) {
-					// if any 1 is not found, go to next rule
-					continue nextCategorisationRule
-				}
-			}
-			// if execution reaches here then all category identifiers were found
-			return rule.Category, rule.CategoryIdentifiers, nil
-		} else {
-			// any identifiers can be found in description
-			matchedIdentifiers := make([]CategoryIdentifier, 0)
-			for _, id := range rule.CategoryIdentifiers {
-				if strings.Contains(description, id.String()) {
-					// mark that one was found
-					matchedIdentifiers = append(matchedIdentifiers, id)
-				}
-			}
-			if len(matchedIdentifiers) > 0 {
-				return rule.Category, matchedIdentifiers, nil
-			}
-		}
-	}
-
-	return "", nil, ErrCouldNotClassify{Reason: "not match"}
-}
-
-var CategorisationRules = []CategoryRule{
-	{
-		CategoryIdentifiers: []CategoryIdentifier{
-			"electricity",
-			"fee",
-		},
-		Category: ElectricityCategory,
-		Strict:   true,
-	},
-	{
-		CategoryIdentifiers: []CategoryIdentifier{
-			"wesbank",
-		},
-		Category: CarRepaymentCategory,
-	},
-	{
-		CategoryIdentifiers: []CategoryIdentifier{
-			"vod",
-			"prepaid",
-		},
-		Category: CellphoneAirtimeDataCategory,
-		Strict:   true,
-	},
-	{
-		CategoryIdentifiers: []CategoryIdentifier{
-			"telkommobi",
-		},
-		Category: InternetCategory,
-	},
-	{
-		CategoryIdentifiers: []CategoryIdentifier{
-			"disc",
-			"prem",
-			"medical",
-		},
-		Category: MedicalAidCategory,
-		Strict:   true,
-	},
-	{
-		CategoryIdentifiers: []CategoryIdentifier{
-			"salary",
-			"andile",
-		},
-		Category: SalaryCategory,
-		Strict:   true,
-	},
 }
