8cc9839bd8b302f298666f3d3bb798920ce27033
Bernard <brbitzbussy@gmail.com>
2020-02-29 17:41:04 +0200

    confirm that budget category rule exists

--- internal/pkg/budget/entry/validator/basic/validator.go
+++ internal/pkg/budget/entry/validator/basic/validator.go
@@ -3,6 +3,9 @@ package basic
 import (
 	budgetEntryCategoryRuleStore "github.com/BRBussy/bizzle/internal/pkg/budget/entry/categoryRule/store"
 	budgetEntryValidator "github.com/BRBussy/bizzle/internal/pkg/budget/entry/validator"
+	bizzleException "github.com/BRBussy/bizzle/internal/pkg/exception"
+	"github.com/BRBussy/bizzle/internal/pkg/mongo"
+	"github.com/BRBussy/bizzle/pkg/validate/reasonInvalid"
 	validationValidator "github.com/BRBussy/bizzle/pkg/validate/validator"
 	"github.com/rs/zerolog/log"
 )
@@ -27,6 +30,32 @@ func (v *validator) ValidateForCreate(request *budgetEntryValidator.ValidateForC
 	if err := v.validator.Validate(request); err != nil {
 		log.Error().Err(err)
 		return nil, err
+	}
+
+	reasonsInvalid := make(reasonInvalid.ReasonsInvalid, 0)
+
+	if request.BudgetEntry.CategoryRuleID != "" {
+		// if category rule ID is not blank, this user should be able to retrieve it
+		if _, err := v.budgetEntryCategoryRuleStore.FindOne(&budgetEntryCategoryRuleStore.FindOneRequest{
+			Claims:     request.Claims,
+			Identifier: request.BudgetEntry.CategoryRuleID,
+		}); err != nil {
+			switch err.(type) {
+			case mongo.ErrNotFound:
+				reasonsInvalid = append(
+					reasonsInvalid,
+					reasonInvalid.ReasonInvalid{
+						Field: "categoryRuleID",
+						Type:  reasonInvalid.DoesntExist,
+						Help:  "must exist",
+						Data:  request.BudgetEntry.CategoryRuleID,
+					},
+				)
+			default:
+				log.Error().Err(err).Msg("unable to retrieve budget category rule")
+				return nil, bizzleException.ErrUnexpected{}
+			}
+		}
 	}
 
 	return &budgetEntryValidator.ValidateForCreateResponse{}, nil
