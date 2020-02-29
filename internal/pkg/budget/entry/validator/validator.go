777da5663fdbf68313027b241dd465a55a43e14d
Bernard <brbitzbussy@gmail.com>
2020-02-29 16:45:27 +0200

    populate validator request and response objects

--- internal/pkg/budget/entry/validator/validator.go
+++ internal/pkg/budget/entry/validator/validator.go
@@ -1,5 +1,10 @@
 package validator
 
+import (
+	budgetEntry "github.com/BRBussy/bizzle/internal/pkg/budget/entry"
+	"github.com/BRBussy/bizzle/pkg/validate/reasonInvalid"
+)
+
 // Validator provides validation functions for budget entries
 type Validator interface {
 	ValidateForCreate(*ValidateForCreateRequest) (*ValidateForCreateResponse, error)
@@ -8,20 +13,22 @@ type Validator interface {
 
 // ValidateForCreateRequest is the request object for the ValidateForCreate service
 type ValidateForCreateRequest struct {
-
+	Claims        	claims.Claims       `validate:"required"`
+	BudgetEntry 	budgetEntry.Entry
 }
 
 // ValidateForCreateResponse is the response object for the ValidateForCreate service
 type ValidateForCreateResponse struct {
-
+	ReasonsInvalid reasonInvalid.ReasonsInvalid
 }
 
 // ValidateForUpdateRequest is the request object for the ValidateForUpdate service
 type ValidateForUpdateRequest struct {
-
+	Claims        	claims.Claims       `validate:"required"`
+	BudgetEntry 	budgetEntry.Entry
 }
 
 // ValidateForUpdateResponse is the response object for the ValidateForUpdate service
 type ValidateForUpdateResponse struct {
-
+	ReasonsInvalid reasonInvalid.ReasonsInvalid
 }
